4c0b90b5e92201b6d593a6c665b17d0bc0e65b60
Byron Wall <byron@byroni.us>
2026-02-25 22:29:56 -0500

    Add console log capture comp

---
+++ app/src/components/console-log-capture/index.ts
@@ -0,0 +1,12 @@
+export { ConsoleLogCapturePanel } from "./ConsoleLogCapturePanel";
+export {
+  clearConsoleLogEntries,
+  consoleLogCaptureEnabled,
+  consoleLogCaptureMaxEntries,
+  consoleLogEntries,
+  initializeConsoleLogCapture,
+  setConsoleLogCaptureEnabled,
+  setConsoleLogCaptureMaxEntries,
+  type ConsoleCaptureEntry,
+  type ConsoleCaptureLevel,
+} from "./consoleLogCapture.store";
